df6bde6c8e4a8f3869f881ebc92b59567614a691
Sai Achyuth Konda <saiachyuthkonda@gmail.com>
2024-11-24 22:36:54 -0600

    modified

---
+++ src/main/java/com/bandiClasses/DMS/DogManagementSystem.java
@@ -0,0 +1,18 @@
+package com.bandiclasses.DMS;
+
+import org.springframework.boot.SpringApplication;
+
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class DogManagementSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagementSystem.class, args);
+	}
+
+}
---
+++ src/test/java/com/greeshmaClasses/DMS/DogsManagementSystemApplicationTests.java
@@ -0,0 +1,18 @@
+package com.greeshmaClasses.DMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 
+ * 
+ * @author Greeshma Borra - S567537
+ */
+@SpringBootTest
+class DogsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
